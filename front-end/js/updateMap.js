67caa4a80dc623c72b4d94a7ef339fa4a695b2e7
Igor Silva <asilvaigor@gmail.com>
2020-12-02 22:20:38 +0100

    Fix filterTrajectoriesByDate

--- front-end/js/updateMap.js
+++ front-end/js/updateMap.js
@@ -3,7 +3,23 @@ const drawTrajectories = (airportsPairListWithCounts) => {
 }
 
 const filterTrajectoriesByDate = (start, end) => {
-    return ctx.routesCounts.filter(({ date }) => start < date && date < end)
+    const routesMap = new Map()
+    ctx.routesCounts
+        .filter(({ date }) =>
+            start && end ? start < date && date < end : true
+        )
+        .forEach(({ origin_airport, destination_airport, count }) => {
+            const key = origin_airport + destination_airport
+            routesMap.set(
+                key,
+                (routesMap.has(key) ? routesMap.get(key) : 0) + parseInt(count)
+            )
+        })
+    return Array.from(routesMap.entries()).map(([key, value]) => ({
+        origin_airport: key.substring(0, 4),
+        destination_airport: key.substring(4, 8),
+        count: value,
+    }))
 }
 
 const updateMap = (start, end) => {
