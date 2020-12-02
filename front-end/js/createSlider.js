c4f883b2c3e327013a7cfdb738fc0b64d235f5e1
Igor Silva <asilvaigor@gmail.com>
2020-12-02 18:45:19 +0100

    Add slider

---
+++ front-end/js/createSlider.js
@@ -0,0 +1,44 @@
+const makeSlider = () => {
+    const HEIGHT = 100
+
+    // Create map with dates and counts
+    var weeksCounts = new Map()
+    ctx.routesCounts.forEach(({ date, count }) => {
+        const cur = weeksCounts.has(date) ? weeksCounts.get(date) : 0
+        weeksCounts.set(date, cur + parseInt(count))
+    })
+    weeksCounts = Array.from(weeksCounts.entries()).map(([key, value]) => ({
+        date: key,
+        count: value,
+    }))
+
+    // Vega-lite spec for the slider graph
+    var vlSpec = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+        data: { values: weeksCounts },
+        width: ctx.w,
+        height: HEIGHT,
+        mark: 'area',
+        selection: { brush: { type: 'interval', encodings: ['x'] } },
+        encoding: {
+            x: {
+                field: 'date',
+                type: 'temporal',
+                axis: { grid: false },
+            },
+            y: {
+                field: 'count',
+                type: 'quantitative',
+                axis: { tickCount: 4 },
+            },
+            color: {
+                value: 'rgb(71, 69, 67)',
+            },
+        },
+    }
+
+    // Create element
+    sliderG = d3.select('#main').append('g').attr('id', 'slider')
+    var vlOpts = { width: ctx.w, height: HEIGHT, actions: false }
+    vegaEmbed('#slider', vlSpec, vlOpts)
+}
--- front-end/js/main.js
+++ front-end/js/main.js
@@ -3,29 +3,38 @@
 var ctx = {
     w: 960,
     h: 484,
-    panZoomMode: true
-};
+    panZoomMode: true,
+}
 
-var createViz = function(){
-    console.log("Using D3 v"+d3.version);
-    var svgEl = d3.select("#main").append("svg");
-    svgEl.attr("width", ctx.w);
-    svgEl.attr("height", ctx.h);
-    loadData(svgEl);
-};
+var createViz = () => {
+    console.log('Using D3 v' + d3.version)
+    var svgEl = d3.select('#main').append('svg')
+    svgEl.attr('width', ctx.w)
+    svgEl.attr('height', ctx.h)
+    loadData(svgEl)
+}
 
-var loadData = function(svgEl){
+var loadData = svgEl => {
     // Load data, transform it, store it in ctx
-    var loadCountries = d3.json("ne_50m_admin_0_countries.geojson")
-    var loadBrazilianStates = d3.json("brazil_states.geojson") 
+    var loadCountries = d3.json('ne_50m_admin_0_countries.geojson')
+    var loadBrazilianStates = d3.json('brazil_states.geojson')
+    var loadAirports = d3.csv('filtered_airports.csv')
+    var loadRoutesCounts = d3.csv('routes_counts_small.csv')
 
     // Executing loads and then calling makeMap
-    Promise.all([loadCountries, loadBrazilianStates])
-    .then((values) => {
+    Promise.all([
+        loadCountries,
+        loadBrazilianStates,
+        loadAirports,
+        loadRoutesCounts,
+    ]).then(values => {
         ctx.countries = values[0].features
         ctx.states = values[1].features
-        
+        ctx.airports = values[2]
+        ctx.routesCounts = values[3]
+
         // Drawing map
         makeMap(svgEl)
+        makeSlider()
     })
-};
+}
