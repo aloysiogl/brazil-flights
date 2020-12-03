5ec95af7cecafb2d967b0214e184ce6764c40c33
Igor Silva <asilvaigor@gmail.com>
2020-12-03 01:40:09 +0100

    Change slider colors

--- front-end/js/createSlider.js
+++ front-end/js/createSlider.js
@@ -1,7 +1,11 @@
 const makeSlider = () => {
     const HEIGHT = 100
-    const AXIS_WIDTH = 49
+    const AXIS_WIDTH = 14
     const AXIS_HEIGHT = 36
+    const BACKGROUND_COLOR = 'rgb(24,26,27)'
+    const GRID_COLOR = 'rgb(52, 51, 50)'
+    const LINE_COLOR = 'rgba(9, 255, 243, .75)'
+    const LABEL_COLOR = 'lightgray'
 
     // Create map with dates and counts
     var weeksCounts = new Map()
@@ -20,31 +24,43 @@ const makeSlider = () => {
         data: { values: weeksCounts },
         width: ctx.w - AXIS_WIDTH,
         height: HEIGHT - AXIS_HEIGHT,
-        mark: 'area',
-        selection: { brush: { type: 'interval', encodings: ['x'] } },
+        mark: 'line',
+        selection: {
+            brush: { type: 'interval', encodings: ['x'] },
+        },
+        background: BACKGROUND_COLOR,
+        config: { view: { stroke: GRID_COLOR } },
         encoding: {
             x: {
                 field: 'date',
                 type: 'temporal',
                 title: null,
-                axis: { grid: false },
+                axis: {
+                    grid: false,
+                    domainColor: GRID_COLOR,
+                    tickColor: GRID_COLOR,
+                    labelColor: LABEL_COLOR,
+                },
             },
             y: {
                 field: 'count',
                 type: 'quantitative',
-                title: 'Count',
-                axis: { tickCount: 4, format: '~s' },
+                title: null,
+                axis: {
+                    tickCount: 4,
+                    gridColor: GRID_COLOR,
+                    domainColor: GRID_COLOR,
+                    labels: false,
+                    ticks: false,
+                },
             },
-            color: { value: 'rgb(71, 69, 67)' },
+            color: { value: LINE_COLOR },
         },
     }
 
     // Create element
-    sliderG = d3
-        .select('#main')
-        .append('g')
-        .attr('id', 'slider')
-    var vlOpts = { autosize: { type: 'fit', contains: 'padding' }, actions: false }
+    sliderG = d3.select('#main').append('g').attr('id', 'slider')
+    var vlOpts = { actions: false }
     vegaEmbed('#slider', vlSpec, vlOpts).then(({ _, view }) => {
         configureEventListener(view)
     })
@@ -58,7 +74,7 @@ const configureEventListener = view => {
 
         ctx.currentDateSelection = {
             start: startDate,
-            end: endDate
+            end: endDate,
         }
 
         updateMap(startDate, endDate)
