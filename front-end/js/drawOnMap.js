bf9cd59da1daa0bf851b1641836db5f6d729baa0
Aloysio <aloysiogl@gmail.com>
2020-12-04 20:41:50 +0100

    Fix worng plane position

--- front-end/js/drawOnMap.js
+++ front-end/js/drawOnMap.js
@@ -143,9 +143,9 @@ const drawPlanes = routesCountsList => {
             const r = - Math.atan2(-y, x) * 180 / Math.PI;
 
             // Gettin scale
-            const s = Math.min(Math.sin(Math.PI * t) * 0.07, 0.05);
+            const s = Math.min(Math.sin(Math.PI * t) * 0.07, 0.01);
 
-            return `translate(${p[0]},${p[1]}) rotate(${r}) scale(${s})`
+            return `translate(${p[0]-288*s},${p[1]-256*s}) rotate(${r}, ${288*s}, ${256*s}) scale(${s})`
         }
     }
 
@@ -157,7 +157,7 @@ const drawPlanes = routesCountsList => {
             Math.random() *
             samples.reduce((sum, { weight }) => sum + weight, 0);
 
-        // first sample n where sum of weight for [0..n] > sample
+        // First sample n where sum of weight for [0..n] > sample
         const { value } = samples.find(
             ({ weight }) => (sample -= weight) < 0
         );
@@ -180,49 +180,46 @@ const drawPlanes = routesCountsList => {
         var plane = ctx.planesGroup
             .data([{traj: traj, route: route}])
             .append("path")
-            .attr("d", "M480 192H365.71L260.61 8.06A16.014 16.014 0 0 0 246.71 0h-65.5c-10.63 0-18.3 10.17-15.38 20.39L214.86 192H112l-43.2-57.6c-3.02-4.03-7.77-6.4-12.8-6.4H16.01C5.6 128-2.04 137.78.49 147.88L32 256 .49 364.12C-2.04 374.22 5.6 384 16.01 384H56c5.04 0 9.78-2.37 12.8-6.4L112 320h102.86l-49.03 171.6c-2.92 10.22 4.75 20.4 15.38 20.4h65.5c5.74 0 11.04-3.08 13.89-8.06L365.71 320H480c35.35 0 96-28.65 96-64s-60.65-64-96-64z")
-            .attr("fill", "red")   
-            // .on("mouseover", d => console.log("click"))
-            //  (d, e) => {
-            //     console.log("test")
-            //     console.log(e)
-            // }
-            // )
+            // Code for the svg plane
+            .attr("d", "M480 192H365.71L260.61 8.06A16.014 16.014 0 0 0 246.71 0h-65.5c-10.63 0-18.3 10.17-15.38"+
+                       " 20.39L214.86 192H112l-43.2-57.6c-3.02-4.03-7.77-6.4-12.8-6.4H16.01C5.6 128-2.04 137.78."+
+                       "49 147.88L32 256 .49 364.12C-2.04 374.22 5.6 384 16.01 384H56c5.04 0 9.78-2.37 12.8-6.4L"+
+                       "112 320h102.86l-49.03 171.6c-2.92 10.22 4.75 20.4 15.38 20.4h65.5c5.74 0 11.04-3.08 13.8"+
+                       "9-8.06L365.71 320H480c35.35 0 96-28.65 96-64s-60.65-64-96-64z")
+            .attr("class", "base_plane")
 
-        var logMsg = d => (t) => {
-            // d3.select("#about_plane").html(t)
-            // console.log(d)
-        }
+        // Show plane information after mouse hover
+        plane.on("mouseover", (e, d) => {
+            if (ctx.selectedPlane)
+                ctx.selectedPlane.attr("class", "base_plane")
+            plane.attr("class", "selected_plane")
 
-        // plane.append('title').text("im a plane")
-        var func = (e, d) => {
-            if (ctx.selectedPlane){
-                ctx.selectedPlane.style("fill", "red")
-                // clearInterval(ctx.intervalSelectedPlane)
-            }
-            plane.style("fill", "white")
+            // Information in the dialog
             d3.select("#about_plane").html(
                     `Origin: ${d.route.origin_airport}\n`+
                     `Destination: ${d.route.destination_airport}\n`
 
                 )
-            // ctx.intervalSelectedPlane = setInterval(logMsg(e), 300)
-            ctx.selectedPlane = plane;
-        }
+            ctx.selectedPlane = plane
+        })
 
-        plane.on("mouseover", func)
-
+        // Plane movement
         plane
             .transition()
             .duration(10000)
             .attrTween("transform", t => tween(t.traj[0], t.traj[1])())
+            .on("end", d => {
+                // If the plane was the selected plane clear the selection
+                if (plane == ctx.selectedPlane)
+                    d3.select("#about_plane").html("")
+            })
             .remove()
     }
 
     // Conditions for spawning new planes
     if (routesCountsList.length > 0){
         clearInterval(ctx.intervalSpawnPlanesId)
-        ctx.intervalSpawnPlanesId = setInterval(spawnPlane, 1000)
+        ctx.intervalSpawnPlanesId = setInterval(spawnPlane, 100)
     }
     else {
         clearInterval(ctx.intervalSpawnPlanesId)
